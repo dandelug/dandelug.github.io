4c5d8bf6accd78f50d3177ddb3ddc46cbe782831
ticketshowjjrt <ticketshowjjrt@gmail.com>
2021-09-06 19:41:31 -0500

    v3.0

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+type objServ = {
+  nombre?: string;
+  img?: string;
+  ruta?: string;
+};
+
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  styleUrls: ['./servicios.component.css'],
 })
 export class ServiciosComponent implements OnInit {
+  lisServicios: objServ[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.lisServicios = [
+      {
+        nombre: 'Consultoría contable',
+        img: '../../../assets/images/servicios/contable.jpg',
+        ruta: 'consContable'
+      },
+      {
+        nombre: 'Consultoría tributaria',
+        img: '../../../assets/images/servicios/tributaria.jpg',
+        ruta: 'consTributaria'
+      },
+      {
+        nombre: 'Reingeniería de costos de producción',
+        img: '../../../assets/images/servicios/reingenieria.jpeg',
+        ruta: 'reinCostos',
+      },
+      {
+        nombre: 'Auditoría externa e interna',
+        img: '../../../assets/images/servicios/auditoria.jpg',
+        ruta: 'audiExtInt',
+      },
+      {
+        nombre: 'Evaluaciones financieras',
+        img: '../../../assets/images/servicios/evaluaciones.jpg',
+        ruta: 'evalFinancieras',
+      },
+      {
+        nombre: 'Lavado de activos',
+        img: '../../../assets/images/servicios/lavado.jpg',
+        ruta: 'lavaActivos',
+      },
+      {
+        nombre: 'Capacitaciones y desarrollo profesional',
+        img: '../../../assets/images/servicios/capacitaciones.jpeg',
+        ruta: 'capacitaciones',
+      },
+      {
+        nombre: 'Servicios legales societarios',
+        img: '../../../assets/images/servicios/legales.jpg',
+        ruta: 'servLegales',
+      }
+    ];
   }
 
+  ngOnInit(): void {}
 }
